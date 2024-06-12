3361d9b45b0abcbc91746745f61d73ab5fa15e23
Bruno Vieira Correa <bruno345viera@gmail.com>
2024-06-12 13:50:29 -0300

    confia no processo

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Details from "./src/pages/Details";
+import NewTask from "./src/pages/NewTask";
+import Task from "./src/pages/Task";
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Task' component={Task} />
+        <Stack.Screen name='Details' component={Details} />
+        <Stack.Screen name='NewTask' component={NewTask} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
---
+++ src/pages/NewTask.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+
+export default function NewTask() {
+  return (
+    <View>
+      <Text>NewTask</Text>
+    </View>
+  );
+}
